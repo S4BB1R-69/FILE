dbb6061b7a63ca574cb688b49cc7343dfe3f1292
Mohammad Raj <122721728+S4BB1R-69@users.noreply.github.com>
2023-05-04 23:28:59 +0600

    Update mew.py

--- mew.py
+++ mew.py
@@ -1,23 +1,2 @@
-#coding=utf-8
-import os, sys, platform
-os.system('rm -rf MEW.so')
- 
-try:
-    if sys.argv[1]=='update':
-        os.system('rm -rf MEW.so')
-except:
-    pass
- 
- 
-bit = platform.architecture()[0]
-if bit == '64bit':
-    if not os.path.isfile('MEW.so'):
-        os.system('curl -L https://github.com/S4BB1R-69/FILE/blob/main/MEW.cpython-311.so?raw=true -o MEW.so') 
-        print("\1b[1;92mWELCOME TO MY NEW TOOLS ")
-        import MEW
-    else:
-        import MEW
-        
- 
-elif bit == '32bit':
-    print(" SORRY 32 BIT NOT WORKING ")
+import sys
+print("Tools Is Updating Broh. Wait as you can.😊")
